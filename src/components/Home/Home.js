dda0f7f12ff219d8d1f44ce98c75c4c5773c18da
jomoljaison <jomoljaison99@gmail.com>
2022-03-16 18:13:57 +0530

    Metamask finished

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import Card from "../UI/Card/Card";
+import classes from "./Home.module.css";
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <Card className={classes.home}>
+      <h1>Welcome</h1>
+      <p>{props.currentAccount}</p>
+      <p>{props.balance} ETH</p>
+    </Card>
+  );
+};
+export default Home;
